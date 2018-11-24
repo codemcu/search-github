8049d81bc6a7a94176d9dfdda4c40881a8f0bc68
Mauricio Correa <frontend.mcu@gmail.com>
2018-11-24 21:27:10 +0100

    index, css, js, img, gitignore

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function () {
+
+  /* global console*/
+  /* jshint node: true */
+
+  "use strict";
+
+  const input = document.querySelector('input');
+  const container = document.querySelector('.container');
+
+})();
