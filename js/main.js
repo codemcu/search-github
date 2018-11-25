5ea2127ba01c92f39314dd3a746361effd4c7a01
Mauricio Correa <frontend.mcu@gmail.com>
2018-11-25 10:36:07 +0100

    addEventListener (searchUser), fetch, trim

--- js/main.js
+++ js/main.js
@@ -6,6 +6,38 @@
   "use strict";
 
   const input = document.querySelector('input');
+  input.addEventListener('keyup', searchUser, false);
   const container = document.querySelector('.container');
+
+  function searchUser($event) {
+
+    if ($event.code === 'Enter' && $event.isTrusted) {
+
+      if (input.value.trim() === '') {
+
+        const p = document.createElement('P');
+        container.appendChild(p);
+        p.textContent = 'Please enter username';
+        return false;
+
+      } else {
+
+        if (document.querySelector('ul') !== null) {
+          document.querySelector('ul').remove();
+        }
+
+        const text = input.value.trim();
+
+        fetch(`https://api.github.com/search/users?q=${text}+in%3Afullname&type=Users`)
+          //.then(handleErrors)
+          .then(data => data.json())
+          .then(res => {
+            console.log(res.items);
+          });
+
+      }
+    }
+
+  }
 
 })();
