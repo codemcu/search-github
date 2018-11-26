b06e8df321a906f3f1f8f083720baf4f8c5b372a
Mauricio Correa <frontend.mcu@gmail.com>
2018-11-26 23:50:56 +0100

    fetch followers/followings

--- js/main.js
+++ js/main.js
@@ -33,6 +33,60 @@
           .then(data => data.json())
           .then(res => {
             console.log(res.items);
+
+            var ul = document.createElement('UL');
+            container.appendChild(ul);
+            var resArray = res.items;
+
+            resArray.forEach(function(item) {
+
+              var li = document.createElement('LI');
+              var img = document.createElement('IMG');
+              var div = document.createElement('DIV');
+              div.setAttribute('class', 'user-details');
+              var spanFollowers = document.createElement('SPAN');
+              var spanFollowings = document.createElement('SPAN');
+              var spanScore = document.createElement('SPAN');
+
+              li.appendChild(document.createTextNode(item.login));
+
+              spanScore.textContent = `Score: ${item.score}`;
+
+              img.setAttribute('src', item.avatar_url);
+
+              ul.appendChild(li);
+              li.appendChild(img);
+              li.appendChild(div);
+              div.appendChild(spanScore);
+
+              // ul.addEventListener('click', function($event) {
+              //   if ($event.target.tagName === 'LI') {
+              //     window.open(item.html_url);
+              //   }
+              // }, false);
+
+              fetch(`https://api.github.com/users/${item.login}/followers`)
+                // .then(handleErrors)
+                .then(function(dataFollowers) {
+                  return dataFollowers.json();
+                })
+                .then(function(resFollowers) {
+                  spanFollowers.textContent = `Followers: ${resFollowers.length}`;
+                  div.appendChild(spanFollowers);
+
+                });
+
+              fetch(`https://api.github.com/users/${item.login}/following`)
+                // .then(handleErrors)
+                .then(function(dataFollowings) {
+                  return dataFollowings.json();
+                })
+                .then(function(resFollowings) {
+                  spanFollowings.textContent = `Followings: ${resFollowings.length}`;
+                  div.appendChild(spanFollowings);
+
+                });
+            });
           });
 
       }
