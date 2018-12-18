df4f592323ba0b2046e69dea81422343535b819e
Mauricio Correa <frontend.mcu@gmail.com>
2018-12-18 21:52:07 +0100

    async/await

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 (function () {
 
-  /* global console*/
-  /* jshint node: true */
-
-  "use strict";
-
   const input = document.querySelector('input');
   input.addEventListener('keyup', searchUser, false);
   const container = document.querySelector('.container');
 
-  function searchUser($event) {
+  async function searchUser($event) {
 
     if ($event.code === 'Enter' && $event.isTrusted) {
 
-      if (input.value.trim() === '') {
+      if (!input.value.trim()) {
 
         const p = document.createElement('P');
         container.appendChild(p);
@@ -22,76 +17,78 @@
 
       } else {
 
-        if (document.querySelector('ul') !== null) {
+        const isUl = document.querySelector('ul') !== null;
+
+        if (isUl) {
           document.querySelector('ul').remove();
         }
 
         const text = input.value.trim();
 
-        fetch(`https://api.github.com/search/users?q=${text}+in%3Afullname&type=Users`)
-          //.then(handleErrors)
-          .then(data => data.json())
-          .then(res => {
-            console.log(res.items);
-
-            const ul = document.createElement('UL');
-            container.appendChild(ul);
-            const resArray = res.items;
-
-            resArray.forEach(function(item) {
-
-              const li = document.createElement('LI');
-              const img = document.createElement('IMG');
-              const div = document.createElement('DIV');
-              div.setAttribute('class', 'user-details');
-              const divFollowers = document.createElement('DIV');
-              const divFollowings = document.createElement('DIV');
-              const divScore = document.createElement('DIV');
-
-              li.appendChild(document.createTextNode(item.login));
-
-              divScore.innerHTML = `Score: <span>${item.score.toFixed(2)}</span>`;
-
-              img.setAttribute('src', item.avatar_url);
-
-              ul.appendChild(li);
-              li.appendChild(img);
-              li.appendChild(div);
-              div.appendChild(divScore);
-
-              // ul.addEventListener('click', function($event) {
-              //   if ($event.target.tagName === 'LI') {
-              //     window.open(item.html_url);
-              //   }
-              // }, false);
-
-              fetch(`https://api.github.com/users/${item.login}/followers`)
-                // .then(handleErrors)
-                .then(function(dataFollowers) {
-                  return dataFollowers.json();
-                })
-                .then(function(resFollowers) {
-                  divFollowers.innerHTML = `Followers: <span>${resFollowers.length}</span>`;
-                  div.appendChild(divFollowers);
-
-                });
-
-              fetch(`https://api.github.com/users/${item.login}/following`)
-                // .then(handleErrors)
-                .then(function(dataFollowings) {
-                  return dataFollowings.json();
-                })
-                .then(function(resFollowings) {
-                  divFollowings.innerHTML = `Followings: <span>${resFollowings.length}</span>`;
-                  div.appendChild(divFollowings);
-
-                });
-            });
-          });
+        const response = await callApi(`https://api.github.com/search/users?q=${text}+in%3Afullname&type=Users`);
+        generateUserList(response);
 
       }
     }
 
+  }
+
+  function ajax(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(response => {
+          if (response.status === 200) {
+            resolve(response.json())
+          } else {
+            reject(response.json());
+          }
+        })
+        .catch(error => console.log(error));
+    })
+  }
+
+  async function callUserProfile(url) {
+    return await ajax(url);
+  }
+
+  async function callApi(url) {
+    return await ajax(url);
+  }
+
+  function generateUserList(data) {
+    const ul = document.createElement('UL');
+    container.appendChild(ul);
+
+    const resArray = data.items;
+
+    resArray.forEach(async function (item) {
+      const urls = item.url;
+      const netWorking = await callUserProfile(urls);
+      const div = makeTemplate(item, netWorking);
+      ul.insertAdjacentHTML('afterbegin', div);
+
+    })
+  }
+
+  function makeTemplate(item, netWorking) {
+    const template = `
+      <li>${item.login}
+        <img src="${item.avatar_url}">
+        <div class="user-details">
+          <div>Score: 
+            <span>${item.score.toFixed(2)}</span>
+          </div>
+          <div>
+            <span>Followers: ${netWorking.followers}</span>
+          </div>
+          <div>
+            <span>Followings: ${netWorking.following}</span>
+          </div>
+        </div>
+       </li>
+    `;
+
+    return template;
   }
 
 })();
